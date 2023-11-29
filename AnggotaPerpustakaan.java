d3bf9ee750c9b0f45eccb37fc605ff38beb1d384
robbyhdayatt <112568253+robbyhdayatt@users.noreply.github.com>
2023-11-29 18:38:01 +0700

    AnggotaPerpustakaan.java

--- AnggotaPerpustakaan.java
+++ AnggotaPerpustakaan.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package perpustakaanxyz;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Hafizh
- */
 public class AnggotaPerpustakaan {
         private String nama;
         private String nomorAnggota;
