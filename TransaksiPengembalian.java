9a80df0ce0afd5f3c6084ee1d271ce4f5c8dc241
robbyhdayatt <112568253+robbyhdayatt@users.noreply.github.com>
2023-11-29 18:41:05 +0700

    TransaksiPengembalian.java

--- TransaksiPengembalian.java
+++ TransaksiPengembalian.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package perpustakaanxyz;
 
-/**
- *
- * @author Hafizh
- */
 public class TransaksiPengembalian{
     public AnggotaPerpustakaan anggota;
     public Buku buku;
@@ -21,7 +12,6 @@ public class TransaksiPengembalian{
     }
 
     public void displayInfo(){
-//        String tanggalKembali = new Date().toString();
         System.out.println("Transaksi Pengembalian:");
         System.out.println("Nama\t\t\t: " + anggota.getNama());
         System.out.println("Buku\t\t\t: '" + buku.getJudul()+"'");
